2ab5fb272b25fdbeade397517800afff34ed2574
Kaio Marques Fonseca <kaio.fonseca@codeplan.df.gov.br>
2023-04-28 11:55:16 -0300

    feat: add funcoes (add task)

--- build/index.js
+++ build/index.js
@@ -3,16 +3,36 @@
 const input = document.querySelector('.textInsert');
 const headerButtonCheck = document.querySelector('.checkAllSelected');
 const headerButtonRemove = document.querySelector('.removeAllSelected');
-const addTaskButton = document.querySelector('.newTaskButton');
+const form = document.querySelector('.new-task-container');
 const list = document.querySelector('.list');
-addTaskButton === null || addTaskButton === void 0 ? void 0 : addTaskButton.addEventListener('click', e => {
+form === null || form === void 0 ? void 0 : form.addEventListener('submit', e => {
     e.preventDefault();
-    if ((input === null || input === void 0 ? void 0 : input.value) === '' || (input === null || input === void 0 ? void 0 : input.value) === null)
-        return;
-    const task = {
-        id: crypto.randomUUID(),
-        title: input === null || input === void 0 ? void 0 : input.value,
-        completed: false,
-        createdAt: new Date()
-    };
+    if ((input === null || input === void 0 ? void 0 : input.value) === '' || (input === null || input === void 0 ? void 0 : input.value) === null) {
+        input.classList.add("error");
+        alert('Preencha o campo corretamente!');
+    }
+    else {
+        input.classList.remove("error");
+        const newTask = {
+            id: crypto.randomUUID(),
+            title: input === null || input === void 0 ? void 0 : input.value,
+            completed: false,
+            createdAt: new Date()
+        };
+        addListItem(newTask);
+        input === null || input === void 0 ? void 0 : input.value = "";
+    }
 });
+function addListItem(task) {
+    const item = document.createElement("li");
+    const label = document.createElement("label");
+    const checkbox = document.createElement("input");
+    const button = document.createElement("button");
+    const span = document.createElement("span");
+    checkbox.type = "checkbox";
+    checkbox.checked = task.completed;
+    span.append(task.title);
+    label.append(checkbox, span, button);
+    item.append(label);
+    list === null || list === void 0 ? void 0 : list.append(item);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,49 @@
 
+type Task = {
+    id: string, 
+    title: string, 
+    completed: boolean, 
+    createdAt: Date
+}
+
 // Selecionando os elementos e tipando o HTML.
 const input = document.querySelector<HTMLInputElement>('.textInsert')
 const headerButtonCheck = document.querySelector<HTMLDivElement>('.checkAllSelected')
 const headerButtonRemove = document.querySelector<HTMLDivElement>('.removeAllSelected')
-const addTaskButton = document.querySelector<HTMLDivElement>('.newTaskButton')
+const form = document.querySelector<HTMLFormElement>('.new-task-container')
 const list = document.querySelector<HTMLUListElement>('.list')
 
-addTaskButton?.addEventListener('click', e => {
+form?.addEventListener('submit', e => {
     e.preventDefault()
 
-    if(input?.value === '' || input?.value === null) return
-
-    const task = {
-        id: crypto.randomUUID(),
-        title: input?.value,
-        completed: false,
-        createdAt: new Date()
-
+    if(input?.value === '' || input?.value === null) {
+        input.classList.add("error")
+        alert('Preencha o campo corretamente!')
+    } else {
+        input.classList.remove("error")
+        const newTask: Task = {
+            id: crypto.randomUUID(),
+            title: input?.value,
+            completed: false,
+            createdAt: new Date()
+        }
+    
+        addListItem(newTask)
+        input?.value = ""
     }
+
 })
+
+function addListItem(task: Task) {
+    const item = document.createElement("li")
+    const label = document.createElement("label")
+    const checkbox = document.createElement("input")
+    const button = document.createElement("button")
+    const span = document.createElement("span")
+    checkbox.type = "checkbox"
+    checkbox.checked = task.completed
+    span.append(task.title)
+    label.append(checkbox, span, button)
+    item.append(label)
+    list?.append(item)
+}
